cb142d056cf5a59a5190d95739882e78d674b3e7
Léona Chevrel <chevrel.leona@gmail.com>
2018-08-23 15:00:03 +0200

    wip AJAX

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,15 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+function loadBody(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "body.js.erb", true);
+  xhttp.send();
+}
